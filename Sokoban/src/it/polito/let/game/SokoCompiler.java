de1eee8b41423025f804153751c2c470e9af2f2d
wolfmanu@hotmail.it <wolfmanu@hotmail.it@362566e4-3882-11de-9d05-31133e6853b1>
2009-06-11 15:27:03 +0000

    Finished

--- Sokoban/src/it/polito/let/game/SokoCompiler.java
+++ Sokoban/src/it/polito/let/game/SokoCompiler.java
@@ -1,4 +1,5 @@
 package it.polito.let.game;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -13,9 +14,6 @@ import javax.swing.JTextArea;
 import it.polito.let.generated.Lexer;
 import it.polito.let.generated.SokoParserCup;
 import it.polito.let.util.SokoPieces;
-
-
-
 
 public class SokoCompiler
 {
@@ -57,7 +55,6 @@ public class SokoCompiler
         	return;
         }
        
-        
         SokoPieces[][] mappa = p.getMap();
 		if(!p.HasError()){
         	if(checkIntegrity(mappa) && colorMap(mappa)){
@@ -87,9 +84,7 @@ public class SokoCompiler
     		msg.setRows(1);
     		msg.setEditable(false);
         	barraframe.setVisible(true);
-        	
         }
-        	
     }
     catch (Exception e) {
         e.printStackTrace();
@@ -98,29 +93,9 @@ public class SokoCompiler
    
 	private static boolean colorMap(SokoPieces[][] map) {
 		
-		
 		msg.append("Checkig map bounds...\n");
 		barraframe.repaint();
-		//PiecesAndBool[][] coloredMap=new PiecesAndBool[h+2][w+2];
-		/*boolean[][] coloredMap= new boolean[h][w];
-		for(int i=0; i<h ; i++)
-			for(int j=0; j<w ; j++){
-				System.out.println(i+" "+j);
-				//coloredMap[i+1][j+1]=new PiecesAndBool();
-				//coloredMap[i+1][j+1].piece=map[i][j];
-				//coloredMap[i+1][j+1].visited=false;
-				coloredMap[i][j]=false;
-			}*/
-		/*for(int i=0; i<h+2; i++){
-			coloredMap[i][0].piece=SokoPieces.blank;
-			coloredMap[i][w+1].piece=SokoPieces.blank;
-		}
-		for(int i=0; i<w+2; i++){
-			coloredMap[0][i].piece=SokoPieces.blank;
-			coloredMap[h+1][i].piece=SokoPieces.blank;
-		}*/
 		
-		//if((coloredMap=checkBounds(coloredMap, 1 , 1))==null){
 		for(int i=0; i<map.length; i++){		//controllo a partire da tutti i punti del bordo
 			if((map=checkBounds(map, i , 0))==null){ 
 				msg.append("Checking bounds FAILED1!");
@@ -146,46 +121,20 @@ public class SokoCompiler
 		return true;
 	}
 
-	/*private static PiecesAndBool[][] checkBounds(PiecesAndBool[][] coloredMap, int posx, int posy) {
-		int offq,offr;
-		
-		for(offq=-1; offq<=1; offq++)
-			for(offr=-1; offr<=1; offr++)
-				if (offq!=0 && offr!=0 && offr!=offq){
-					if (!coloredMap[posx+offq][posy+offr].visited)
-						if(coloredMap[posx+offq][posy+offr].piece==SokoPieces.blank)
-							continue;
-						else if(coloredMap[posx+offq][posy+offr].piece==SokoPieces.wall)
-							coloredMap[posx+offq][posy+offr].visited=true;
-						else if(coloredMap[posx+offq][posy+offr].piece==SokoPieces.floor){
-							coloredMap[posx+offq][posy+offr].piece=SokoPieces.blank;
-							coloredMap[posx+offq][posy+offr].visited=true;
-							if(checkBounds(coloredMap, posx+offq, posy+offr)==null)
-								return null;
-						}
-						else return null;							
-				}
-		return coloredMap;
-	}
-*/
 	private static SokoPieces[][] checkBounds(SokoPieces[][] Map, int posx, int posy) {
 		int[] offq={-1, 0, 0, 0, 1};
 		int[] offr={0, -1, 0, 1, 0};
-		System.out.println("entro checkbounds, "+posx+", "+posy);
-		//for(offq=-1; offq<=1; offq++)
-			//for(offr=-1; offr<=1; offr++)
+
 		if(Map[posx][posy]==SokoPieces.wall)
 			return Map;
+		
 		for(int i=0; i<5; i++)
 				if ( (posx+offq[i])>=0 && (posy+offr[i])>=0 && 
 						(posx+offq[i])<Map.length && (posy+offr[i])<Map[0].length){
-					//System.out.println("Checking Map["+(posx+offq[i])+"]["+(posy+offr[i])+"]="+Map[posx+offq[i]][posy+offr[i]].name());
 					if(Map[posx+offq[i]][posy+offr[i]]!=SokoPieces.blank && 
 							Map[posx+offq[i]][posy+offr[i]]!=SokoPieces.wall){
 						if(Map[posx+offq[i]][posy+offr[i]]==SokoPieces.floor){
 							Map[posx+offq[i]][posy+offr[i]]=SokoPieces.blank;
-							//coloredMap[posx+offq][posy+offr]=true;
-							//System.out.println("Richiamo");
 							if((Map=checkBounds(Map, posx+offq[i], posy+offr[i]))==null)
 								return null;
 						}
@@ -205,17 +154,11 @@ public class SokoCompiler
 		int goal=0, box=0, me=0;
 		boolean err=false;
 		
-		msg=new JTextArea("Checking map integrity...\n");
+		msg=new JTextArea("Checking map integrity...");
 		msg.setEditable(false);
-		msg.setRows(1);
-		//JProgressBar progressBar = new JProgressBar(0, 100);
-		//barraframe.getContentPane().add(progressBar); 
 		barraframe.getContentPane().add(msg); 
-		//JTextField msg=new JTextField();
 		msg.setEditable(false);
     	barraframe.setVisible(true);
-		
-		//progressBar.setValue(k);
 		
 		for(int i=0; i<map.length; i++)
 			for(int j=0; j<map[i].length; j++){
@@ -225,44 +168,36 @@ public class SokoCompiler
 					goal++;
 				if(map[i][j]==SokoPieces.me || map[i][j]==SokoPieces.megoal)
 					me++;
-				//progressBar.setValue((k++)/(map.length*map[0].length));
 			}
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {}
 		if(me<1){
 			msg.append("Too fiew sokoban defined");
-			//System.err.println("Too many sokoban defined");
 			err=true;
 		}
 		if(me>1){
 			msg.append("Too many sokoban defined");
-			//System.err.println("Too many sokoban defined");
 			err=true;
 		}
 		if(goal<p.blocchiMobili){
 			msg.append("Too fiew goal defined");
-			//System.err.println("Too fiew goal defined");
 			err=true;
 		}
 		if(goal>p.blocchiMobili){
 			msg.append("Too many goal defined");
-			//System.err.println("Too may goal defined");
 			err=true;
 		}
 		if(box<p.blocchiMobili){
 			msg.append("Too fiew box defined");
-			//System.err.println("Too fiew box defined "+box+ goal);
 			err=true;
 		}
 		if(box>p.blocchiMobili){
 			msg.append("Too many box defined");
-			//System.err.println("Too may box defined");
 			err=true;
 		}
 		if(err){
 			msg.setForeground(Color.RED);
-			//barraframe.getContentPane().add(msg);
 			msg.append("\nMap integrity FAILED!");
 			barraframe.repaint();
 			return false;
